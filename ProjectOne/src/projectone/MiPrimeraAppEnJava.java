a49153650372d03d1f2f12e1fd974e758ce4de27
Carlos Pedraza <carlospedraza@usco.edu.co>
2024-08-15 09:54:34 -0500

    Primeros pasos

---
+++ ProjectOne/src/projectone/MiPrimeraAppEnJava.java
@@ -0,0 +1,10 @@
+package projectone; //Crea una carpeta con el nombre de la clase
+import javax.swing.*; //Importo toda las libreria swing.
+
+public class MiPrimeraAppEnJava {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Hola Mundo!"); //Muestro en pantalla el mensaje "Hola Mundo!"
+    }
+}
---
+++ ProjectOne/src/projectone/ProjectOne.java
@@ -0,0 +1,15 @@
+package projectone; //Esto es un comentario de línea
+/**
+ *
+ * @author CARLOS PEDRAZA 2023456678
+ */
+public class ProjectOne {
+    //Atributos de clase
+    //Métodos
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java!!!");
+    }
+    
+}
+
